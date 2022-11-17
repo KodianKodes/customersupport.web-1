9995e24640ac28c6c5b06db574619bf0e9a5b4fa
tochibedford <musicofbyte@gmail.com>
2022-11-17 20:20:12 +0100

    setup: included prettier

--- scrybe_frontend/vite.config.js
+++ scrybe_frontend/vite.config.js
@@ -1,17 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import eslint from '@rollup/plugin-eslint';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import eslint from "@rollup/plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: true,
   },
-  plugins: [react(), {
-    ...eslint({
-      include: 'src/**/*.+(js|jsx|ts|tsx)',
-    }),
-    enforce: 'pre',
-    apply: 'build',
-  }],
+  plugins: [
+    react(),
+    {
+      ...eslint({
+        include: "src/**/*.+(js|jsx|ts|tsx)",
+      }),
+      enforce: "pre",
+      apply: "build",
+    },
+  ],
 });
