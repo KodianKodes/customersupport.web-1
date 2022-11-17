5feb1612164f913832d731146c19cf844eabb356
tochibedford <musicofbyte@gmail.com>
2022-11-17 20:06:08 +0100

    setup: added eslint to project

--- scrybe_frontend/src/App.jsx
+++ scrybe_frontend/src/App.jsx
@@ -1,15 +1,18 @@
-import './App.scss'
-import scrybeLogo from './assets/scrybe_logo_with_text.svg'
+import React from 'react';
+import './App.scss';
+import scrybeLogo from './assets/scrybe_logo_with_text.svg';
 
 function App() {
-
   return (
     <div className="App">
-      <div style={{width: "100vw", height: "80vh", display: "grid", placeItems: "center"}}>
-        <img src={scrybeLogo} alt="scrybeLogo" style={{objectFit: "contain", width: "100%"}}/>
+      <div style={{
+        width: '100vw', height: '80vh', display: 'grid', placeItems: 'center',
+      }}
+      >
+        <img src={scrybeLogo} alt="scrybeLogo" style={{ objectFit: 'contain', width: '100%' }} />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- scrybe_frontend/vite.config.js
+++ scrybe_frontend/vite.config.js
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import eslint from '@rollup/plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: true
+    host: true,
   },
-  plugins: [react()]
-})
+  plugins: [react(), {
+    ...eslint({
+      include: 'src/**/*.+(js|jsx|ts|tsx)',
+    }),
+    enforce: 'pre',
+    apply: 'build',
+  }],
+});
