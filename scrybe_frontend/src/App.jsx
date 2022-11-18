b88ba956cf0698ac400f1fda490aaf7691f28ee2
dprincecoder <dprincecoder@gmail.com>
2022-11-18 19:54:06 +0100

    fixing routing issues

--- scrybe_frontend/src/App.jsx
+++ scrybe_frontend/src/App.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.scss";
-import Dummy from "./components/Dummy";
+// import Dummy from "./components/Dummy";
 import UploadedRecordings from "./components/UploadRecordings";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Dummy />} />
-      <Route
-        exact
-        path="/uploaded-recordings"
-        element={<UploadedRecordings />}
-      />
+      {/* <Route path="/" element={<Dummy />} /> */}
+      <Route exact path="/" element={<UploadedRecordings />} />
     </Routes>
   );
 }
