ab2201c3cd7a650ac6c0a2fb0156ece7cd3e126e
webala <webd01@protonmail.com>
2022-11-18 22:08:27 +0300

    feat/create policy page

--- scrybe_frontend/src/App.jsx
+++ scrybe_frontend/src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.scss";
 import { Routes, Route } from "react-router-dom";
 import Dummy from "./components/Dummy";
+import PrivacyPolicy from "./pages/PrivacyPolicy/PrivacyPolicy";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Dummy />} />
+      <Route path="/privacy-policy" element={<PrivacyPolicy />} />
     </Routes>
   );
 }
---
+++ scrybe_frontend/src/pages/PrivacyPolicy/Components/ContactUs.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ContactUs() {
+  return (
+    <div className='sub-container'>
+        <h1>19. Contact Us</h1>
+        <p>If you have any questions about this Privacy Policy, please contact us by email: info@scrybe.com.</p>
+    </div>
+  )
+}
+
+export default ContactUs
