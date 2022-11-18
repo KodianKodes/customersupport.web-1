b7a2a74947883876cecafd22d1eac85cc1db081b
rambey-coder <olayiwolaagboolaakanji@gmail.com>
2022-11-18 12:17:15 +0100

    feat: (FRO-20) project init

--- scrybe_frontend/src/App.jsx
+++ scrybe_frontend/src/App.jsx
@@ -1,24 +1,11 @@
 import React from "react";
 import "./App.scss";
-import scrybeLogo from "./assets/scrybe_logo_with_text.svg";
+import Services from "./services-page/Services";
 
 function App() {
   return (
     <div className="App" data-testid="app-container">
-      <div
-        style={{
-          width: "100vw",
-          height: "80vh",
-          display: "grid",
-          placeItems: "center",
-        }}
-      >
-        <img
-          src={scrybeLogo}
-          alt="scrybeLogo"
-          style={{ objectFit: "contain", width: "100%" }}
-        />
-      </div>
+      <Services />
     </div>
   );
 }
