0b3b768453b6a2f3560dd09685f4bdae006d3d17
MAV3R1CK <eddymav247@gmail.com>
2022-11-18 18:17:34 +0100

    agent report page setup

--- scrybe_frontend/src/App.jsx
+++ scrybe_frontend/src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.scss";
 import { Routes, Route } from "react-router-dom";
 import Dummy from "./components/Dummy";
+import AgentReport from "./pages/agent-report/";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Dummy />} />
+      <Route path="/agent-report" element={<AgentReport />} />
     </Routes>
   );
 }
