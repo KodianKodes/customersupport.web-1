d8de88decbc416fa3594c26dad4e4c48ab1e8bb2
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2022-11-19 16:26:25 +0100

    feat: Add logout route

--- scrybe_frontend/src/App.jsx
+++ scrybe_frontend/src/App.jsx
@@ -3,12 +3,14 @@ import "./App.scss";
 import { Routes, Route } from "react-router-dom";
 import Dummy from "./components/Dummy";
 import Account from "./pages/Account";
+import Logout from "./pages/Logout";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Dummy />} />
       <Route path="/account" element={<Account />} />
+      <Route path="/logout" element={<Logout />} />
     </Routes>
   );
 }
