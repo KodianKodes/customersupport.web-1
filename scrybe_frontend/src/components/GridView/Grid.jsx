2f9fe180b3ebf670778088692e4633d0ab4ba36f
wineshuga <uziwinnie@gmail.com>
2022-11-19 11:59:30 +0100

    feat(FRO-13): upload premade files

---
+++ scrybe_frontend/src/components/GridView/Grid.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable */
+import React from "react";
+import CallImage from "../../assets/callImage.png";
+import styles from "./grid.module.scss";
+
+function Grid(props) {
+  return (
+    <div className={styles.card}>
+      <div className={styles.filename}>
+        <img src={CallImage} alt="" />
+        <div className={styles.filename__text}>
+          <h5>{props.name}</h5>
+          <p className={styles.paragraph}>{props.lenght}</p>
+        </div>
+      </div>
+      <div className={styles.analysis}>
+        <h5 className={styles.main__analysis}>{props.analysis}</h5>
+        <p className={styles.paragraph}>{props.date}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Grid;
---
+++ scrybe_frontend/src/components/ListHeader/ListHeader.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable */
+import React from "react";
+import styles from "./listHeader.module.scss";
+
+const ListHeader = () => {
+  return (
+    <div className={styles.header}>
+      <table className={styles.table}>
+        <tr>
+          <th>File Name</th>
+          <th>Agent</th>
+          <th>Sentiment Result</th>
+          <th>Date Uploaded</th>
+          <th>Lenght</th>
+        </tr>
+      </table>
+    </div>
+  );
+};
+
+export default ListHeader;
