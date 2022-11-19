76a1127180cd9f3c3845427e65ec225fbfe3bc55
damolabolaji <bolaji.oyindamola@gmail.com>
2022-11-19 19:07:10 +0100

    chore: cleaning up routing if industry pag

--- scrybe_frontend/src/App.jsx
+++ scrybe_frontend/src/App.jsx
@@ -3,11 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import SentimentAnalysis from "./Pages/SentimentAnalysis";
 import DummyPage from "./Pages/DummyPage";
 import TranscribePage from "./Pages/TranscribePage/TranscribePage";
-import { FAQs, TermsOfService } from "./pages";
+import { FAQs, TermsOfService } from "./Pages";
 import LandingPage from "./Pages/landing-page-a";
 import PageB from "./Pages/Landing-page-b";
 import NavBar from "./components/navBar";
 import TermsAndCondition from "./Pages/TermsAndCondition/TermsOfService";
+import Industry from "./Pages/indsutry/industry";
+import HowItWorks from "./Pages/HowItWorks/HowItWorks";
 
 import Services from "./Pages/services-page/Services";
 import Solutions from "./Pages/solutions-page/Solutions";
@@ -15,102 +17,101 @@ import DashboardOverview from "./Pages/DashboardOverview";
 
 function App() {
   return (
-    <Routes>
-      {/* landing page */}
-      <Route path="/" element={<DummyPage />} />
-      {/* sentiment analysis */}
-      <Route
-        path="/sentiment-analysis"
-        element={<DummyPage someText="sentiment-analysis" />}
-      />
-      {/* transcriptions */}
-      <Route
-        path="/transicriptions"
-        element={<DummyPage someText="transcriptions" />}
-      />
-      {/* try for free */}
-      <Route path="/try" element={<DummyPage someText="try for free" />} />
-      {/* press */}
-      <Route path="/press" element={<DummyPage someText="press" />} />
-      {/* settings */}
-      <Route path="/settings" element={<DummyPage someText="settings" />} />
-      {/* uploaded */}
-      <Route
-        path="/uploaded"
-        element={<DummyPage someText="uploaded recordings" />}
-      />
-      {/* history */}
-      <Route path="/history" element={<DummyPage someText="history" />} />
-      {/* help and support */}
-      <Route path="/help" element={<DummyPage someText="help and supprt" />} />
-      {/* sign in */}
-      <Route
-        path="/auth"
-        element={<DummyPage someText="sign-in, sign up, reset password" />}
-      />
-      <Route
-        path="/transicriptions"
-        element={<DummyPage someText="transcriptions" />}
-      />
-      <Route
-        path="/transicriptions"
-        element={<DummyPage someText="transcriptions" />}
-      />
-      {/* career page & legal */}
-      <Route
-        path="/careers-and-legal"
-        element={<DummyPage someText="career page and legal" />}
-      />
-      {/* solutions & services */}
-      <Route
-        path="/services"
-        element={<DummyPage someText="solutions and services" />}
-      />
-      {/* about us */}
-      <Route path="/about-us" element={<DummyPage someText="about us" />} />
-      {/* events */}
-      <Route path="/events" element={<DummyPage someText="events" />} />
-      {/* agent report */}
-      <Route
-        path="/report"
-        element={<DummyPage someText="report and performance" />}
-      />
-      {/* upload pages */}
-      <Route path="/upload" element={<DummyPage someText="upload pages" />} />
-      {/* dashboard */}
-      <Route
-        path="/dashboard"
-        element={<DummyPage someText="dashboard overview" />}
-      />
-      {/* accounts */}
-      <Route path="/accounts" element={<DummyPage someText="accounts" />} />
-      {/* industry */}
-      <Route path="/industry" element={<DummyPage someText="industry" />} />
-      {/* demo pages */}
-      <Route path="/demos" element={<DummyPage someText="demo pages" />} />
-      {/* reviews & pricing */}
-      <Route
-        path="/reviews-and-pricing"
-        element={<DummyPage someText="reviews and pricing" />}
-      />
-      {/* leaderboard */}
-      <Route
-        path="/leaderboard"
-        element={<DummyPage someText="leaderboard" />}
-      />
-      {/* terms and conditions */}
-      <Route
-        path="/terms"
-        element={<DummyPage someText="terms and conditions" />}
-      />
-      {/* terms of service */}
-      <Route
-        path="/terms-of-service"
-        element={<DummyPage someText="terms of service" />}
-      />
-      {/* blog */}
-      <Route path="/blog" element={<DummyPage someText="blog" />} />
-    </Routes>
+    <>
+      <NavBar />
+      <Routes>
+        {/* landing page */}
+        <Route path="/" element={<LandingPage />} />
+        {/* sentiment analysis */}
+        <Route path="/sentiment-analysis" element={<SentimentAnalysis />} />
+        {/* transcriptions */}
+        <Route path="/transcriptions" element={<TranscribePage />} />
+        {/* try for free */}
+        <Route path="/try" element={<DummyPage someText="try for free" />} />
+        {/* press */}
+        <Route path="/press" element={<DummyPage someText="press" />} />
+        {/* settings */}
+        <Route path="/settings" element={<DummyPage someText="settings" />} />
+        {/* uploaded */}
+        <Route
+          path="/uploaded"
+          element={<DummyPage someText="uploaded recordings" />}
+        />
+        {/* history */}
+        <Route path="/history" element={<DummyPage someText="history" />} />
+        {/* help and support */}
+        <Route
+          path="/help"
+          element={<DummyPage someText="help and supprt" />}
+        />
+        {/* sign in */}
+        <Route
+          path="/auth"
+          element={<DummyPage someText="sign-in, sign up, reset password" />}
+        />
+        {/* career page & legal */}
+        <Route
+          path="/careers-and-legal"
+          element={<DummyPage someText="career page and legal" />}
+        />
+        {/* solutions & services */}
+        <Route
+          path="/services"
+          element={<Services someText="solutions and services" />}
+        />
+
+        <Route
+          path="/solutions"
+          element={<Solutions someText="solutions and services" />}
+        />
+        {/* about us */}
+        <Route path="/about-us" element={<DummyPage someText="about us" />} />
+        {/* events */}
+        <Route path="/events" element={<DummyPage someText="events" />} />
+        {/* agent report */}
+        <Route
+          path="/report"
+          element={<DummyPage someText="report and performance" />}
+        />
+        {/* upload pages */}
+        <Route path="/upload" element={<DummyPage someText="upload pages" />} />
+        {/* dashboard */}
+        <Route path="/dashboard" element={<DashboardOverview />} />
+        {/* accounts */}
+        <Route path="/accounts" element={<DummyPage someText="accounts" />} />
+        {/* industry */}
+        <Route path="/industry" element={<Industry someText="industry" />} />
+        {/* How It Works */}
+        <Route path="/industry" element={<HowItWorks someText="industry" />} />
+        {/* demo pages */}
+        <Route path="/demos" element={<DummyPage someText="demo pages" />} />
+        {/* reviews & pricing */}
+        <Route
+          path="/reviews-and-pricing"
+          element={<DummyPage someText="reviews and pricing" />}
+        />
+        {/* leaderboard */}
+        <Route
+          path="/leaderboard"
+          element={<DummyPage someText="leaderboard" />}
+        />
+        {/* terms and conditions */}
+        <Route
+          path="/terms"
+          element={<TermsAndCondition someText="terms and conditions" />}
+        />
+        {/* terms of service */}
+        <Route
+          path="/terms-of-service"
+          element={<DummyPage someText="terms of service" />}
+        />
+        {/* blog */}
+        <Route path="/blog" element={<DummyPage someText="blog" />} />
+        <Route path="/homeB" element={<PageB />} />
+        <Route path="faq" element={<FAQs />} />
+        <Route path="faq" element={<TermsOfService />} />
+      </Routes>
+    </>
   );
 }
 
