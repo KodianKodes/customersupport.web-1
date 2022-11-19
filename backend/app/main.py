187a971fe754ee117c68ca7092e635fd6eb7e2f0
David Akwuru <daakwuru@gmail.com>
2022-11-19 14:53:19 +0000

    Feat(BAC-8): added login endpoint to process authentication or login script

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,13 @@
-from fastapi import Depends, FastAPI, UploadFile
+from fastapi import Depends, FastAPI, UploadFile, status, HTTPException
 from routers.sentiment import sentiment
 import models
+from jwt import (
+    main_login
+)
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
+from db import Base, engine, SessionLocal
+from sqlalchemy.orm import Session
 
 description = """
 Scrybe API helps you analyse sentiments in your customer support calls
@@ -16,6 +23,17 @@ tags_metadata = [
         "description": "Operations with users. The **login** logic is also here.",
     },
 ]
+
+# create the database.
+Base.metadata.create_all(engine)
+
+# database. 
+def get_session():
+    session = SessionLocal()
+    try:
+        yield session
+    finally:
+        session.close()
 
 app = FastAPI(
     title="Scrybe API",
@@ -35,3 +53,9 @@ async def analyse(files: UploadFile):
     transcript = transcribe(file)
     sentiment_result = sentiment(transcript)
     return {"transcript": transcript, "sentiment_result": sentiment_result}
+
+# create the endpoint
+@app.post('/login', summary = "create access token for logged in user")
+async def login(form_data: OAuth2PasswordRequestForm = Depends(), session: Session = Depends(get_session)):
+    # return token once the user has been successfully authenticated, or it returns an error.
+    return await main_login(form_data, session)
