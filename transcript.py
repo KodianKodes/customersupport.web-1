a860c187a9e13ee467f88938f5a582362c073a92
Tamunonengiyeofori <tkennwariso@gmail.com>
2022-11-19 14:19:44 +0100

    feat(BAC-12): added script to transcibe audio file

--- transcript.py
+++ transcript.py
@@ -1,25 +1,25 @@
+from fastapi import FastAPI
+from io import BytesIO
+import base64
+import banana_dev as banana
+from dotenv import load_dotenv
+import os
 
-from fastapi import APIRouter
+# Load all environment variables
+load_dotenv()
 
-transcribe_router = APIRouter(
-    prefix = "transcribe",
-    tags = ["TRANSRIPTION_SERVICE"]
-)
+filename = input("Enter the filepath to your audio file")
 
-@transcribe_router.post("/")
-def transcribe_audio():
-    ...
-    
-    
-    
-    
-    
-    
+api_key = os.getenv("API_KEY")
+model_key = os.getenv("MODEL_KEY")
 
-# import whisper 
+def convert_file(filename):
+# Expects an mp3 file named test.mp3 in directory
+    with open(filename, "rb") as file:
+        # Load audio file
+        mp3bytes = BytesIO(file.read())
+        mp3 = base64.b64encode(mp3bytes.getvalue()).decode("ISO-8859-1")
+        model_payload = {"mp3BytesString": mp3}
+        out = banana.run(api_key, model_key, model_payload)
+        return out
 
-# audio_file = ""
-
-# model = whisper.load_model("base")
-# result = model.transcribe(audio_file)
-# print(result["text"])
