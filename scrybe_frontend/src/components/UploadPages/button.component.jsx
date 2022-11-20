ec448ba4d7718cfaa810e7312740f4b89f4b0e57
Tlamz <78260378+Tlamz@users.noreply.github.com>
2022-11-20 19:42:48 +0100

    Delete scrybe_frontend/src/components/UploadPages directory

--- scrybe_frontend/src/components/UploadPages/button.component.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export function PrimaryButton({ onClick, text = 'PrimaryButton' }) {
-  return (<button className='primary-button' onClick={() => onClick()}>{text}</button>)
-}
