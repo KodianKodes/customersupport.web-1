295717b18737eb34e86a5f4906597b59ed3be81a
Tochukwu Bedford <34871260+tochibedford@users.noreply.github.com>
2022-11-20 13:24:05 +0100

    Revert "Revert "Feat/fro 15 scrybe help and support page""

--- scrybe_frontend/src/App.jsx
+++ scrybe_frontend/src/App.jsx
@@ -11,6 +11,12 @@ import History from "./pages/History/History";
 import Services from "./pages/services-page/Services";
 import Solutions from "./pages/solutions-page/Solutions";
 import DashboardOverview from "./pages/DashboardOverview";
+import HelpSupport from "./pages/HelpAndSupport/HelpSupport";
+import General from "./components/HelpAndSupport/HelpAndSupportGeneral/General";
+import General1 from "./components/HelpAndSupport/general1/General1";
+import General2 from "./components/HelpAndSupport/general2/General2";
+import PromotedArticles from "./components/HelpAndSupport/promoted-articles/PromotedArticles";
+import PromotedArticle1 from "./components/HelpAndSupport/PromotedArticle1/PromotedArticle1";
 import About from "./pages/AboutUs/About";
 import TryForFree from "./pages/TryForFree";
 import Industry from "./pages/indsutry/industry";
@@ -105,6 +111,26 @@ function App() {
       <Route path="/homeB" element={<PageB />} />
       <Route path="faq" element={<FAQs />} />
       <Route path="faq" element={<TermsOfService />} />
+      {/* HelpAndSupport */}
+      <Route path="/help-support" element={<HelpSupport />} />
+      <Route path="/help-support-general" element={<General />}>
+        <Route index element={<General1 />} />
+        <Route path="articles" element={<General2 />} />
+      </Route>
+      <Route
+        path="/help-support-general/promoted-articles"
+        element={<PromotedArticles />}
+      >
+        <Route index element={<PromotedArticle1 />} />
+        <Route path="*" element={<PromotedArticle1 />} />
+      </Route>
+      <Route
+        path="/help-support/promoted-articles"
+        element={<PromotedArticles />}
+      >
+        <Route index element={<PromotedArticle1 />} />
+        <Route path="*" element={<PromotedArticle1 />} />
+      </Route>
     </Routes>
 
   );
